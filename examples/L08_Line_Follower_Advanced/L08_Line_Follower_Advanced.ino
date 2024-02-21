4aba57ab551c1a89a6626a0f047ebfe9919e318d
Andrew Gafford <88450122+agafford-ST@users.noreply.github.com>
2024-02-21 14:21:00 -0500

    Update L08_Line_Follower_Advanced.ino

--- examples/L08_Line_Follower_Advanced/L08_Line_Follower_Advanced.ino
+++ examples/L08_Line_Follower_Advanced/L08_Line_Follower_Advanced.ino
@@ -1,5 +1,5 @@
 /**************************************************
- * L07_Line_Follower_Basic.ino
+ * L08_Line_Follower_Advanced.ino
  * An example program to make the ExoNaut follow a line
  *
  * Author: Andrew Gafford
@@ -11,17 +11,14 @@
 #include <SpaceTrek_ExoNaut.h>                //include the ExoNaut library
 
 exonaut robot;                                //declare the exonaut object named robot
-//int speed = 50;                               //declare and initialize a variable speed set to the value 50
 uint8_t lineData = 0;
 
 void setup() {                                //the setup() function runs once
-  Serial.begin(115200);                       //Start the serial port so data can be sent to the computer
-  delay(500);
   robot.begin();                              //start the robot object
   robot.set_motor_speed(0,0);                 //set the speed of both motors to ZERO
-  delay(1000);                                //wait 1.0 seconds
-  Serial.println("Starting...");
+  delay(1500);                                //wait 1.0 seconds
 }                                             //end of the setup() function
+
 
 void loop() {
   robot.readLineFollower(lineData);
@@ -36,13 +33,10 @@ void loop() {
       robot.set_motor_speed(6, 40);
       break;
     case 0b0011:                              //Sensor 1 and 2 detect black. Sensor 3 and 4 detect white
-      //what to do?
+      robot.set_motor_speed(-5, 40);
       break;
     case 0b0100:                              //only sensor 3 detects black. Sensor 1, 2 and 4 detect white
       robot.set_motor_speed(40, 6);
-      break;
-    case 0b0101:                              //Sensor 1 and 3 detect black. Sensor 2 and 4 detect white
-      //what to do?
       break;
     case 0b0110:                              //Sensor 2 and 3 detect black. Sensor 1 and 4 detect white
       robot.set_motor_speed(40, 40);
@@ -51,6 +45,20 @@ void loop() {
       //what to do?
       break;
     case 0b1000:                              //only sensor 4 detects black. Sensor 1, 2 and 3 detect white
+      //what to do?
+      break;
+    case 0b1100:                              //Sensor 3 and 4 detect black. Sensor 1 and 2 detect white
+      robot.set_motor_speed(40, -5);
+      break;
+    case 0b1110:                              //Sensor 2, 3 and 4 detect black. Only sensor 1 detects white
+      //what to do?
+      break;
+    case 0b1111:                              //all detect black
+      robot.set_motor_speed(0, 0);
+      break;
+
+    //Not needed
+    case 0b0101:                              //Sensor 1 and 3 detect black. Sensor 2 and 4 detect white
       //what to do?
       break;
     case 0b1001:                              //Sensor 1 and 4 detect black. Sensor 2 and 3 detect white
@@ -62,23 +70,12 @@ void loop() {
     case 0b1011:                              //Sensor 1, 2 and 4 detect black. Only sensor 3 detects white
       //what to do?
       break;
-    case 0b1100:                              //Sensor 3 and 4 detect black. Sensor 1 and 2 detect white
-      //what to do?
-      break;
     case 0b1101:                              //Sensor 1, 3 and 4 detect black. Only sensor 2 detects white
       //what to do?
-      break;
-    case 0b1110:                              //Sensor 2, 3 and 4 detect black. Only sensor 1 detects white
-      //what to do?
-      break;
-    case 0b1111:                              //all detect black
-      robot.set_motor_speed(0, 0);
       break;
     default:                                  //unexpected value in lineData
       robot.set_motor_speed(0, 0);            
       break;
   }
-  delay(10);
-  //Serial.print("0b"); Serial.print(lineData, BIN); Serial.print(" - "); Serial.println(lineData);
-  //delay(490);
+  delay(5);
 }
